d6289254a6336c5f887e66e79bc9b590d0b8b90a
Mistzin07 <arthurviniciossoaresbezerra@gmail.com>
2024-10-17 14:12:23 -0300

    primeiro commit

---
+++ src/app/Sobre/page.js
@@ -0,0 +1,8 @@
+export default function Sobre() {
+    return(
+        <>
+        <h1>Eu sou a página 2</h1>
+        <p>Olá</p>
+        </>
+    )
+} 
---
+++ src/app/components/nav/index.js
@@ -0,0 +1,9 @@
+export default function Nav() {
+    return(
+        <>
+        <p>Eu sou um nav</p>
+        <a href="/">Inicio</a>
+        <a href="/Sobre">Sobre</a>
+        </>
+    )
+}
